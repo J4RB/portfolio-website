c19928866d3d79347090fdf18b44292ef5cd8473
Jacob Bregndahl Larsen <jlarsen2107@gmail.com>
2024-03-29 18:21:06 +0100

    Added first case study and made navbar a component

--- portfolio-website/src/app/home/home.component.ts
+++ portfolio-website/src/app/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CaseStudyComponent } from '../case-study/case-study.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CaseStudyComponent, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
+
 export class HomeComponent {
 
 }
