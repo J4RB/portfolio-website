06eee49ff28999453bfb8f019c0e64c853605c97
Jacob Bregndahl Larsen <jlarsen2107@gmail.com>
2024-04-17 13:28:51 +0200

    Added about page

--- portfolio-website/src/app/about/about.component.ts
+++ portfolio-website/src/app/about/about.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  @ViewChild('aboutText', { static: false }) aboutText!: ElementRef;
+  @ViewChild('lineNums', { static: false }) lineNums!: ElementRef;
 
+  lineNumbers: number[] = [];
+
+  ngOnInit(): void {
+    this.lineNumbers = this.createRange(10, 99);
+  }
+
+  ngAfterViewInit(): void {
+    this.adjustLineNumsHeight();
+  }
+
+  @HostListener('window:resize')
+  onWindowResize() {
+    this.adjustLineNumsHeight();
+  }
+
+  adjustLineNumsHeight(): void {
+    this.lineNums.nativeElement.style.height = this.aboutText.nativeElement.offsetHeight + 'px';
+  }
+
+  createRange(m: number, n: number): number[] {
+    return Array.from({length: n - m + 1}, (_, i) => m + i);
+  }
 }
